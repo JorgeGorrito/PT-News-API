c86366428120475c76aa64c31d5ea460836a10e4
JorgeGorrito <j0rg3.4b3ll4@gmail.com>
2026-03-04 08:47:55 -0500

    feat(application): update create article command to use transactions

--- internal/application/articles/commands/create_article/handler.go
+++ internal/application/articles/commands/create_article/handler.go
@@ -3,6 +3,7 @@ package create_article
 import (
 	"context"
 
+	"github.com/JorgeGorrito/PT-News-API/internal/application/transaction"
 	"github.com/JorgeGorrito/PT-News-API/internal/domain/entities"
 	"github.com/JorgeGorrito/PT-News-API/internal/domain/interfaces"
 )
@@ -10,30 +11,47 @@ import (
 type Handler struct {
 	articleRepo interfaces.ArticleRepository
 	authorRepo  interfaces.AuthorRepository
+	txManager   transaction.Manager
 }
 
-func NewHandler(articleRepo interfaces.ArticleRepository, authorRepo interfaces.AuthorRepository) *Handler {
+func NewHandler(
+	articleRepo interfaces.ArticleRepository,
+	authorRepo interfaces.AuthorRepository,
+	txManager transaction.Manager,
+) *Handler {
 	return &Handler{
 		articleRepo: articleRepo,
 		authorRepo:  authorRepo,
+		txManager:   txManager,
 	}
 }
 
 func (h *Handler) Handle(ctx context.Context, cmd Command) (*Response, error) {
-	_, err := h.authorRepo.FindByID(ctx, cmd.AuthorID)
+	var articleID int64
+
+	err := h.txManager.WithinTransaction(ctx, func(txCtx context.Context) error {
+		_, err := h.authorRepo.FindByID(txCtx, cmd.AuthorID)
+		if err != nil {
+			return err
+		}
+
+		article := entities.NewArticle(cmd.AuthorID, cmd.Title)
+
+		if err := article.SetBody(cmd.Body); err != nil {
+			return err
+		}
+
+		if err := h.articleRepo.Save(txCtx, article); err != nil {
+			return err
+		}
+
+		articleID = article.ID()
+		return nil
+	})
+
 	if err != nil {
 		return nil, err
 	}
 
-	article := entities.NewArticle(cmd.AuthorID, cmd.Title)
-
-	if err := article.SetBody(cmd.Body); err != nil {
-		return nil, err
-	}
-
-	if err := h.articleRepo.Save(ctx, article); err != nil {
-		return nil, err
-	}
-
-	return &Response{ID: article.ID()}, nil
+	return &Response{ID: articleID}, nil
 }
