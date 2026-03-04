cd7d53b288e9d2c3d0cc582f72211dc4d262d339
JorgeGorrito <j0rg3.4b3ll4@gmail.com>
2026-03-04 08:48:21 -0500

    feat(application): update publish article command to use transactions

--- internal/application/articles/commands/publish_article/handler.go
+++ internal/application/articles/commands/publish_article/handler.go
@@ -3,30 +3,46 @@ package publish_article
 import (
 	"context"
 
+	"github.com/JorgeGorrito/PT-News-API/internal/application/transaction"
 	"github.com/JorgeGorrito/PT-News-API/internal/domain/interfaces"
 )
 
 type Handler struct {
 	articleRepo interfaces.ArticleRepository
+	txManager   transaction.Manager
 }
 
-func NewHandler(articleRepo interfaces.ArticleRepository) *Handler {
-	return &Handler{articleRepo: articleRepo}
+func NewHandler(articleRepo interfaces.ArticleRepository, txManager transaction.Manager) *Handler {
+	return &Handler{
+		articleRepo: articleRepo,
+		txManager:   txManager,
+	}
 }
 
 func (h *Handler) Handle(ctx context.Context, cmd Command) (*Response, error) {
-	article, err := h.articleRepo.FindByID(ctx, cmd.ArticleID)
+	var articleID int64
+
+	err := h.txManager.WithinTransaction(ctx, func(txCtx context.Context) error {
+		article, err := h.articleRepo.FindByID(txCtx, cmd.ArticleID)
+		if err != nil {
+			return err
+		}
+
+		if err := article.Publish(); err != nil {
+			return err
+		}
+
+		if err := h.articleRepo.Update(txCtx, article); err != nil {
+			return err
+		}
+
+		articleID = article.ID()
+		return nil
+	})
+
 	if err != nil {
 		return nil, err
 	}
 
-	if err := article.Publish(); err != nil {
-		return nil, err
-	}
-
-	if err := h.articleRepo.Update(ctx, article); err != nil {
-		return nil, err
-	}
-
-	return &Response{ID: article.ID()}, nil
+	return &Response{ID: articleID}, nil
 }
